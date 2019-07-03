ccb2af7fd089cc268bab8697b746617511403fd4
Sergio Rubio <sergio.rubio@grupocmc.es>
2019-07-03 19:10:16 +0200

    authguard example

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor( private router: Router) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
     if (next.url.join() === 'products') {
-      return false;
+      return this.router.parseUrl('/login');
     } else {
       return true;
     }
