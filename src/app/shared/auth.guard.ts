eb8d00e8c0ffded64545f5b5a8fc55a39e0b06e8
Sergio Rubio <sergio.rubio@grupocmc.es>
2019-07-10 19:54:44 +0200

    authGuard

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { SessionService } from './session.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor( private router: Router) {}
+  constructor( private router: Router, private session: SessionService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    console.log(next.url.join());
     if (next.url.join() === 'products') {
-      return this.router.parseUrl('/login');
-    } else {
-      return true;
+      if (!this.session.validSession()) {
+        return this.router.parseUrl('/login');
+      }
     }
+    console.log('--->' + next.url.join());
+    return true;
   }
 }
